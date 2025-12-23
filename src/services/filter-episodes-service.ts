53f2cd892a3a015b7260ff4d7c6f3ca33c379e1b
LuizFelixDev <luizhenriquefelix138@gmail.com>
2025-12-23 15:54:38 -0300

    Refatorando camadas

--- src/services/filter-episodes-service.ts
+++ src/services/filter-episodes-service.ts
@@ -1,8 +1,11 @@
+import { IncomingMessage } from "http";
 import { PodcastModel } from "../models/podcast-model";
 import { repositoryPodcast } from "../repositories/podcasts-repository"
 
-export const serviceFilterEpisodes = async (podcastName: string = ""): Promise<PodcastModel[]>  =>{
-  const data = await repositoryPodcast(podcastName);
+export const serviceFilterEpisodes = async (podcastName: string | undefined) => {
+  const queryString = podcastName?.split("?p=")[1] || "";
+
+  const data = await repositoryPodcast(queryString);
 
   return data;
 }
