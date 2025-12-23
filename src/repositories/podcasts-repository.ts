909416f46fed1268ae9f2fa2bf6dad0ffab5d3d3
LuizFelixDev <luizhenriquefelix138@gmail.com>
2025-12-22 21:16:40 -0300

    Modulo dinâmico

--- src/repositories/podcasts-repository.ts
+++ src/repositories/podcasts-repository.ts
@@ -3,10 +3,10 @@ import path from 'path'
 import { Podcast } from '../models/podcast-model';
 
 
-const pathData = path.join(__dirname, "../repositories");
+const pathData = path.join(__dirname, "../repositories/podcasts.json");
 
 export const repositoryPodcast = async (): Promise<Podcast[]> => {
-  const rawData = fs.readFileSync(pathData, "utf-8");
+  const rawData = fs.readFileSync(pathData, 'utf-8');
   const jsonFile = JSON.parse(rawData);
   return jsonFile;
 }
