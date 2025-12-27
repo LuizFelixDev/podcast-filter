2fa39ac6590b90aac55df54ec0b3920db3e25956
LuizFelixDev <luizhenriquefelix138@gmail.com>
2025-12-27 09:03:52 -0300

    Retirando números mágicos e textos flutuantes

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -1,11 +1,13 @@
 import {IncomingMessage, ServerResponse} from 'http' 
 import { serviceListEpisodes } from '../services/list-episodes-service';
 import { serviceFilterEpisodes } from '../services/filter-episodes-service';
+import { StatusCode } from '../utils/http-status-code';
+import { ContentType } from '../utils/content-type';
 
 export const getListEpisodes = async (req: IncomingMessage, res: ServerResponse) =>{
   const content = await serviceListEpisodes();
 
-  res.writeHead(200, {"content-type": "application/json"})
+  res.writeHead(StatusCode.OK, {"content-type": ContentType.JSON});
   res.end(JSON.stringify(content)); 
 };
 
@@ -13,6 +15,6 @@ export const getListEpisodes = async (req: IncomingMessage, res: ServerResponse)
 export const getFilterEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
   const content = await serviceFilterEpisodes(req.url);
 
-  res.writeHead(200, {"Content-Type": "application/json"});
+  res.writeHead(StatusCode.OK, {"Content-Type": ContentType.JSON});
   res.end(JSON.stringify(content));
 };
---
+++ src/utils/content-type.ts
@@ -0,0 +1,8 @@
+export enum ContentType {
+    JSON = 'application/json',
+    HTML = 'text/html',
+    XML = 'application/xml',
+    FORM_URLENCODED = 'application/x-www-form-urlencoded',
+    MULTIPART_FORM_DATA = 'multipart/form-data',
+    TEXT_PLAIN = 'text/plain'
+}
